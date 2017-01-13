6a79ce7354477678220171cc6e9bc9595f78cc19
Le Tien Tai <letientai299@gmail.com>
2017-01-13 10:33:17 +0700

    Restructure to make latexmk work with minted

--- gulpfile.js
+++ gulpfile.js
@@ -8,26 +8,16 @@ gulp.task('default', ['build']);
  * Call latexmk to build the file.
  */
 gulp.task('build', function () {
-  let latexmk = cp.spawn('latexmk')
-  latexmk.stdout.on('data', function (data) {
-    data.toString().split('\r\n')
-      .forEach(s => console.log('LaTeX - out|', s));
-  })
-
-  latexmk.stderr.on('data', function (data) {
-    if (data.length !== 0) {
-      console.error('LaTeX - err| ' + data.toString().trim());
-    }
-  });
+  gulp.src('src/**/*')
+    .pipe(gulp.dest('dest'))
+    .on('end', make);
 });
 
 /**
  * Delete latexmk and minted output folder.
  */
 gulp.task('clean', function () {
-  del(['build/',
-  '_minted-*'
-  ]).then(paths => {
+  del(['dest']).then(paths => {
     if (paths.length !== 0) {
       console.log('Deleted files and folders:\n',
         paths.forEach(s => s.trim()).join('\n'));
@@ -42,5 +32,21 @@ gulp.task('clean', function () {
  */
 gulp.task('serve', ['build'], function () {
   // spawn pdf viewer
-  gulp.watch('*.tex', ['build']);
+  gulp.watch('src/**/*', ['build']);
 });
+
+function make() {
+  let latexmk = cp.spawn('latexmk', {
+    cwd: 'dest'
+  })
+  latexmk.stdout.on('data', function (data) {
+    data.toString().split('\r\n')
+      .forEach(s => console.log('LaTeX - out|', s));
+  })
+
+  latexmk.stderr.on('data', function (data) {
+    if (data.length !== 0) {
+      console.error('LaTeX - err| ' + data.toString().trim());
+    }
+  });
+}
