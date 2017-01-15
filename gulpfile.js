5536572fca891d7269c52ea4096f14dfade59fd9
Le Tien Tai <letientai299@gmail.com>
2017-01-16 04:48:46 +0700

    Watch for color change and convert svg images

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,14 @@ const del = require('del')
 const cp = require('child_process');
 const debug = require('gulp-debug');
 const gutil = require('gulp-util');
+const replace = require('gulp-replace');
+const glob = require('glob');
 const fs = require('fs');
+
+/**
+ * Files that don't require any special treatment.
+ */
+const SIMPLE_FILES = ['src/*.tex', 'src/images/*', 'src/latexmkrc']
 
 gulp.task('default', ['build']);
 
@@ -31,12 +38,13 @@ gulp.task('serve', ['build'], function () {
   /*
    * Recompile pdf normally on tex and image files change
    */
-  gulp.watch(['src/**/*']).on('change', e => {
-    gulp.src(e.path)
-      .pipe(debug())
-      .pipe(gulp.dest('dest'))
-      .on('end', make);
-  })
+  gulp.watch(SIMPLE_FILES, {base: 'src'})
+    .on('change', e => {
+      gulp.src(e.path)
+        .pipe(debug())
+        .pipe(gulp.dest('dest'))
+        .on('end', make);
+    })
 
   /*
    * Generate the theme files, convert svg color to match with once config.json
@@ -52,7 +60,7 @@ gulp.task('serve', ['build'], function () {
  * Copy everything under src folder into dest, and trigger make on end.
  */
 function copyAndMake() {
-  return gulp.src('src/**/*')
+  return gulp.src(SIMPLE_FILES, {base: 'src'})
     .pipe(gulp.dest('dest'))
     .on('end', make);
 }
@@ -84,17 +92,39 @@ function open(pdfFile) {
 function generateColorTheme(cb) {
   let config = JSON.parse(fs.readFileSync('./src/config.json', 'utf8'));
   let theme = config.theme;
-  let colorThemeTexContent = Object.keys(theme)
-    .map(k => `\\definecolor{${k}}{HTML}{${theme[k].replace('#', '')}}`)
-    .join('\n')
 
   if (cb == null) {
     cb = () => {} ;// no-op
   }
 
-  srcString('colorTheme.tex', colorThemeTexContent)
+  removeSvgTempFileSync();
+
+  // Change svg fill color.
+  gulp.src('src/*.svg')
+    .pipe(replace(/fill:#[0-9a-fA-F]{6}/, `fill:${theme.primaryColor}`))
     .pipe(gulp.dest('dest'))
-    .on('end', cb)
+    .on('end', () => writeNewColorTexFile(theme, cb));
+}
+
+/**
+ * Remove all pdf and pdf_tex file generated from svg file by inkscape
+ */
+function removeSvgTempFileSync() {
+  return glob.sync("src/*.svg").map(f => f.replace('src', 'dest'))
+    .map(f => f.replace('.svg', '.*'))
+    .forEach(f => del(f));
+}
+
+/**
+ *Write tex file, so that color theme will be applied to link and other things.
+ */
+function writeNewColorTexFile(theme, cb) {
+  let colorThemeTexContent = Object.keys(theme)
+    .map(k => `\\definecolor{${k}}{HTML}{${theme[k].replace('#', '')}}`)
+    .join('\n')
+  return srcString('colorTheme.tex', colorThemeTexContent)
+    .pipe(gulp.dest('dest'))
+    .on('end', cb);
 }
 
 /**
