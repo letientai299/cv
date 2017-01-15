a3f3110899eda49827e1b22f17f9f905e2926a0e
Le Tien Tai <letientai299@gmail.com>
2017-01-15 15:17:04 +0700

    Add theme, config gulp to support theme changes

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,7 @@ gulp.task('default', ['build']);
 /**
  * Call latexmk to build the file.
  */
-gulp.task('build', function () {
-  gulp.src('src/**/*')
-    .pipe(gulp.dest('dest'))
-    .on('end', make);
-});
+gulp.task('build', copyAndMake);
 
 /**
  * Delete latexmk and minted output folder.
@@ -25,26 +21,56 @@ gulp.task('clean', function () {
  * Spawn the pdf viewer and recompile whenever *.tex file change.
  */
 gulp.task('serve', ['build'], function () {
-  open('dest/main.pdf');
+  open('dest/main.pdf')
 
-  gulp.watch('src/**/*').on('change', e => {
+  /*
+   * Recompile pdf normally on tex and image files change
+   */
+  gulp.watch(['!src/**/*.sty', 'src/**/*']).on('change', e => {
     gulp.src(e.path)
       .pipe(debug())
       .pipe(gulp.dest('dest'))
       .on('end', make);
   })
+
+  /*
+   * If custom packages changes, we have to clean cache and do a full compile.
+   */
+  gulp.watch(['src/**/*.sty']).on('change', function(e) {
+    // If we remove the whole folder, like what we've done in clean task,
+    // zathura won't be able to reload the new pdf.
+    del(['dest/*']).then(ps =>  {
+      copyAndMake()
+    });
+  });
 });
 
+/**
+ * Copy everything under src folder into dest, and trigger make on end.
+ */
+function copyAndMake() {
+  return gulp.src('src/**/*')
+    .pipe(gulp.dest('dest'))
+    .on('end', make);
+}
+
+/**
+ * Spawn latexmk to build project on dest folder
+ */
 function make() {
-  return cp.spawn('latexmk', {
+  // Use spawnSync to ensure that we always have the pdf ready after make()
+  cp.spawnSync('latexmk', {
     cwd: 'dest'
   })
 }
 
+/**
+ * Open the file using OS command.
+ * Currently support Linux and Window only.
+ */
 function open(pdfFile){
   let command = process.platform.match('win.*') ?
     "start" : "xdg-open";
   let execCommand = `${command} ${pdfFile}`;
-  // console.log(execCommand);
   return cp.exec(execCommand);
 }
